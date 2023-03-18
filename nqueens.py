8ea374522dd6f155c368520639e674de996dd7f3
Zhongyang Zheng <89175460+Joker1581@users.noreply.github.com>
2023-03-18 13:46:41 +0800

    Update nqueens.py

--- nqueens.py
+++ nqueens.py
@@ -6,18 +6,21 @@ def is_valid(row, column, queen_position):
             return False
     return True
 
-def n_queens(row, queen_position, n):
+def n_queens(row, queen_position, n, sol_set):
     if row == n:
+        sol_set.append(queen_position)
         print(queen_position)
         return 
     for i in range(n):
         if is_valid(row, i, queen_position):
             queen_position[row] = i
-            n_queens(row + 1, queen_position, n)
+            n_queens(row + 1, queen_position, n, sol_set)
             queen_position[row] = 0
     
 if __name__ == "__main__":
     n = int(input("请输入皇后的数量: "))
     queen_position = [0 for _ in range(n)]
     row = 0
-    n_queens(row, queen_position, n)
+    sol_set = list()
+    n_queens(row, queen_position, n, sol_set)
+    print(len(sol_set))
